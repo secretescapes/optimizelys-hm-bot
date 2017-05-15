965a5ae765fe743c2d14f6714978d78710152d77
Dario Bertini <berdario@gmail.com>
2017-05-15 21:59:59 +0100

    Implement retrieveWeights and adjustABTests AWS Lambda handlers

--- src/Main.purs
+++ src/Main.purs
@@ -1,68 +1,139 @@
 module Main where
 
 import Prelude
-import AWS.Dynamo (DYNAMO, Order(..), QueryResult(..), Dynaff, defaultQuery, put, query)
+import Data.Map as Map
+import AWS.Dynamo (DYNAMO, Dynaff, DynamoDBRecord(..), Order(..), QueryResult(..), StreamPayload(..), StreamEvent(..), defaultQuery, put, query)
 import AWS.Lambda (LAMBDA, Context, succeed, fail)
-import Control.Monad.Aff (launchAff)
-import Control.Monad.Aff.Class (liftAff)
-import Control.Monad.Aff.Console (CONSOLE, logShow)
+import Control.Monad.Aff (launchAff, Aff)
+import Control.Monad.Aff.Console (CONSOLE, logShow, log)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Now (NOW)
 import Control.Monad.Except (runExceptT)
 import Data.Either (Either(..))
-import Data.Maybe (Maybe(..))
+import Data.Foldable (maximumBy)
+import Data.Foreign (Foreign)
+import Data.Foreign.Class (class IsForeign, read)
+import Data.Foreign.Undefined (Undefined(..))
+import Data.List (List, elemIndex, filter, intercalate, intersect, length, take, concatMap, catMaybes)
+import Data.Map (lookup)
+import Data.Maybe (Maybe(..), maybe)
+import Data.Newtype (unwrap)
+import Data.Optimizely.Experiment (emptyVariation)
+import Data.Traversable (for)
+import Data.Tuple (fst, snd, Tuple(..))
+import Main.Common (variationsSchema, Variations(..), foreignParseToEither)
+import Main.Internal (ExperimentId, Map(..), VariationId(..), currentDateTime, exitShowing, exitWith, retryUntil)
+import Main.Optimizely (retrieveVariations)
+import Main.Slack (notify)
 import Network.HTTP.Affjax (AJAX)
+import Network.Optimizely (updateVariation)
 import Network.Optimizely.Auth (Auth(..))
 import Node.Process (PROCESS, lookupEnv)
 
-import Main.Internal (Map, exitWith, exitShowing, ExperimentId, VariationId, currentDateTime)
-import Main.Optimizely (retrieveVariations)
-import Main.Common (variationsSchema, Variations(..))
-import Main.Slack (notify)
--- import Types (InputType(..))
+type LambdaEff eff = Eff (lambda :: LAMBDA | eff)
 
--- handler :: forall eff. Context -> Foreign -> Eff (lambda :: LAMBDA | eff) Unit
--- handler c d = do
---   process $ readData d
---   pure unit
-
---   where
---     readData :: Foreign -> Either String InputType
---     readData = foreignParseToEither <<< read
-
---     process :: Either String InputType -> Eff (lambda :: LAMBDA | eff) Unit
---     process (Left err) = fail c err
---     process (Right (InputType i))  = succeed c $ i.key
+mkHandler :: forall a eff. (IsForeign a) => (a -> LambdaEff eff Unit) -> Context -> Foreign -> LambdaEff eff Unit
+mkHandler fn context event = process $ foreignParseToEither $ read event
+    where
+        process :: Either String a -> Eff (lambda :: LAMBDA | eff) Unit
+        process (Left err) = fail context err
+        process (Right a)  = do
+            fn a
+            succeed context "OK"
 
 
-f :: forall eff. (Map ExperimentId (Map VariationId Int)) -> Dynaff (console :: CONSOLE, now :: NOW | eff) Unit
-f variationWeights = do
+storeWeights :: forall eff. (Map ExperimentId (Map VariationId Int)) -> Dynaff (now :: NOW | eff) Unit
+storeWeights variationWeights = do
     retrievetime <- currentDateTime
-
     put {tablename: variationsSchema.name, item: Variations {keyname: "variations", retrievetime, variationWeights}}
-    (QueryResult rez) <- query (defaultQuery variationsSchema "variations")
-    liftAff $ logShow $ rez.items
-    -- (QueryResult rez) <- query (defaultQuery aSchema "foo"){limit=Just 1, order=Descending}
-    -- liftAff $ logShow $ rez.items
 
 
-main :: Eff (console :: CONSOLE, now :: NOW, dynamo :: DYNAMO, ajax :: AJAX, err :: EXCEPTION, process :: PROCESS) Unit
-main = do
+
+getValid :: Int -> Variations -> Maybe Variations
+getValid max v@(Variations {variationWeights: Map weights}) =
+    if amountOfPathologicalTests <= max
+        then Just v
+        else Nothing
+    where
+        amountOfPathologicalTests = length $ filter isPathological $ Map.values weights
+
+isPathological :: Map VariationId Int -> Boolean
+isPathological (Map m)
+    = case elemIndex 10000 $ Map.values m of
+        Nothing -> false
+        Just _ -> true
+
+lastVariationsDumps :: forall eff. Int -> Dynaff eff (Array Variations)
+lastVariationsDumps limit = do
+    (QueryResult {items}) <- query (defaultQuery variationsSchema "variations"){limit=Just limit, order=Descending}
+    pure items
+
+max100s :: Int
+max100s = 20
+
+retrieveLastGoodStatus :: forall eff. Dynaff eff (Maybe Variations)
+retrieveLastGoodStatus = retryUntil (getValid max100s) lastVariationsDumps
+
+
+retrieveWeights' :: forall eff. Foreign -> Eff (console :: CONSOLE, now :: NOW, dynamo :: DYNAMO, ajax :: AJAX, err :: EXCEPTION, process :: PROCESS | eff) Unit
+retrieveWeights' _ = withOptiKey $ \tkn -> do
+            variationWeights <- runExceptT $ retrieveVariations tkn
+            case variationWeights of
+                Left err -> exitShowing err
+                Right weights -> storeWeights weights
+
+retrieveWeights :: forall eff. Context -> Foreign -> LambdaEff (console :: CONSOLE, now :: NOW, dynamo :: DYNAMO, ajax :: AJAX, err :: EXCEPTION, process :: PROCESS | eff) Unit
+retrieveWeights = mkHandler retrieveWeights'
+
+withOptiKey :: forall eff. (Auth -> Aff (console :: CONSOLE, process :: PROCESS  | eff) Unit)
+                        -> Eff (console :: CONSOLE, err :: EXCEPTION, process :: PROCESS | eff) Unit
+withOptiKey action = do
     maybeKey <- lookupEnv "OPTIMIZELY_KEY"
     void $ launchAff $ case maybeKey of
         Nothing -> exitWith "No OPTIMIZELY_KEY provided"
-        Just key -> do
-            variationWeights <- runExceptT $ retrieveVariations $ ClassicToken key
-            case variationWeights of
-                Left err -> exitShowing err
-                Right weights -> f weights
+        Just key -> action $ ClassicToken key
 
+adjustABTests' :: forall eff. StreamPayload Variations -> Eff (console :: CONSOLE, now :: NOW, dynamo :: DYNAMO, ajax :: AJAX, err :: EXCEPTION, process :: PROCESS | eff) Unit
+adjustABTests' (StreamPayload events) = withOptiKey \tkn -> void $ for pathologicalVariations (flipBackWeights tkn)
+    where
+        records = map (\(StreamEvent {dynamodb:(DynamoDBRecord rec)}) -> rec) events
+        latestImage = map _.newImage $ maximumBy (\{sequenceNumber: n1} {sequenceNumber: n2} -> compare n1 n2) records
+        pathologicalVariations = maybe Nothing (\x -> flipMaybe x $ getValid max100s x) latestImage -- if no events, everything is fine, but this shouldn't happen
 
--- main :: forall eff. Eff ( "err" :: EXCEPTION, "ajax" :: AJAX, "console" :: CONSOLE | eff) Unit
--- main = void $ launchAff $ do
---   log "here"
---   projects <- getProjects
---   logShow $ runExcept projects
+adjustABTests :: forall eff. Context -> Foreign -> LambdaEff (console :: CONSOLE, now :: NOW, dynamo :: DYNAMO, ajax :: AJAX, err :: EXCEPTION, process :: PROCESS | eff) Unit
+adjustABTests = mkHandler adjustABTests'
+
+flipMaybe :: forall a b. a -> Maybe b -> Maybe a
+flipMaybe _ (Just _) = Nothing
+flipMaybe x Nothing = Just x
+
+flipBackWeights :: forall eff. Auth -> Variations -> Aff (console :: CONSOLE, now :: NOW, dynamo :: DYNAMO, ajax :: AJAX, err :: EXCEPTION, process :: PROCESS | eff) Unit
+flipBackWeights tkn (Variations {retrievetime, variationWeights: newW}) = do
+    lastGood <- retrieveLastGoodStatus
+    case lastGood of
+        Nothing -> log "Unable to retrieve a last-good weight status snapshot"
+        Just (Variations {variationWeights: oldW}) -> updateExperiments tkn oldW $ selectNewExperiments max100s oldW newW
+
+selectNewExperiments :: Int -> Map ExperimentId (Map VariationId Int) -> Map ExperimentId (Map VariationId Int) -> List ExperimentId
+selectNewExperiments max oldWeights newWeights = take toBeFlipped canBeChanged
+    where
+        oldAndCorrect = map fst $ filter (not <<< isPathological <<< snd) $ Map.toList $ unwrap oldWeights
+        newButWrong = map fst $ filter (isPathological <<< snd) $ Map.toList $ unwrap newWeights
+        toBeFlipped = length newButWrong - max
+        -- TODO check for toBeFlipped > length canBeChanged
+        canBeChanged = intersect oldAndCorrect newButWrong
+
+updateExperiments :: forall eff. Auth -> Map ExperimentId (Map VariationId Int) -> List ExperimentId -> Aff (console :: CONSOLE, now :: NOW, dynamo :: DYNAMO, ajax :: AJAX, err :: EXCEPTION, process :: PROCESS | eff) Unit
+updateExperiments tkn (Map weights) experiments = do
+    let foundMsg = (show $ length experiments) <> " tests over the limit of " <> show max100s <> " tests allowed to run at 100%"
+        details =  (intercalate "," $ map show experiments)
+    log $ "Found " <> foundMsg <> ". About to flip back: " <> details
+    let variations = concatMap (Map.toList <<< unwrap) $ catMaybes $ map (flip lookup weights) experiments
+    result <- runExceptT $ for variations \(Tuple (VariationId vid) weight) ->
+                updateVariation vid emptyVariation{weight=Undefined (Just weight)} tkn
+    case result of
+        Left err -> logShow err -- TODO collect the errors, and report successes
+        Right _ -> void $ notify $ "I found " <> foundMsg <> ". I flipped back: " <> details
 
 
--- src/Main/Internal.purs
+++ src/Main/Internal.purs
@@ -1,19 +1,27 @@
 module Main.Internal where
 
 import Data.DateTime.Foreign as ForeignDate
+import Data.List.Lazy as L
 import Data.Map as Map
 import AWS.Dynamo.Classes (class DynamoAttribute, class FromDynamo, readDynamoProp)
+import Control.Alternative (class Applicative)
+import Control.Extend (class Functor)
+import Control.Monad (class Monad)
 import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.Console (CONSOLE, log)
 import Control.Monad.Eff.Class (class MonadEff, liftEff)
 import Control.Monad.Eff.Now (NOW, nowDateTime)
+import Data.Array (catMaybes, head)
 import Data.DateTime.Locale (LocalValue(..))
 import Data.Foreign (F, Foreign, writeObject)
 import Data.Foreign.Class (class AsForeign, class IsForeign, read, readJSON, readProp, write, writeProp)
 import Data.Foreign.Generic (defaultOptions)
+import Data.Foreign.Generic.Types (Options)
 import Data.Foreign.Keys (keys)
 import Data.Generic.Rep (class Generic)
+import Data.List.Lazy.Types (Step(..))
 import Data.Map (fromFoldable)
+import Data.Maybe (Maybe(..))
 import Data.Monoid (class Monoid)
 import Data.Newtype (class Newtype, unwrap, wrap)
 import Data.Optimizely (Experiment, Id, Variation)
@@ -21,7 +29,7 @@ import Data.Traversable (traverse)
 import Data.Tuple (Tuple(..), uncurry)
 import Global.Unsafe (unsafeStringify)
 import Node.Process (exit, PROCESS)
-import Prelude (class Ord, class Show, (=<<), (*>), (<<<), show, ($), map, bind, pure)
+import Prelude (class Ord, class Show, class Eq, (=<<), (*>), (<<<), show, ($), map, bind, pure, (<$>), (*), (-))
 
 exitWith :: forall eff a. String -> Aff (process :: PROCESS, console :: CONSOLE |eff) a
 exitWith s = log s *> (liftEff $ exit 1)
@@ -35,9 +43,11 @@ currentDateTime = do
   (LocalValue _ current) <- liftEff nowDateTime
   pure $ DateTime $ ForeignDate.DateTime $ current
 
+opts :: Options
 opts = defaultOptions{unwrapSingleConstructors=true}
 
 newtype ExperimentId = ExperimentId (Id Experiment)
+derive newtype instance eqExperimentId :: Eq ExperimentId
 derive newtype instance ordExperimentId :: Ord ExperimentId
 derive newtype instance showExperimentId :: Show ExperimentId
 derive newtype instance isForeignExperimentId :: IsForeign ExperimentId
@@ -46,6 +56,7 @@ derive instance newtypeExperimentId :: Newtype ExperimentId _
 
 
 newtype VariationId = VariationId (Id Variation)
+derive newtype instance eqVariationId :: Eq VariationId
 derive newtype instance ordVariationId :: Ord VariationId
 derive newtype instance showVariationId :: Show VariationId
 derive newtype instance isForeignVariationId :: IsForeign VariationId
@@ -95,3 +106,27 @@ instance asForeignMap :: (AsForeign k, AsForeign v) => AsForeign (Map k v) where
 instance dynamoAttrMap :: (IsForeign k, Ord k, DynamoAttribute v) => DynamoAttribute (Map k v) where
     dynamoType _ = "M"
 
+
+twoPows :: L.List Int
+twoPows = L.iterate (_ * 2) 1
+
+retryIncreasingly' :: forall a m. (Functor m, Applicative m) => Int -> (a -> Maybe a) -> (Int -> m a) -> m (Maybe a)
+retryIncreasingly' maxattempts selector f = pickIfAny maxattempts selector <$> traverse f twoPows
+
+
+pickIfAny :: forall a. Int -> (a -> Maybe a) -> L.List a -> Maybe a
+pickIfAny maxattempts selector = L.head <<< L.catMaybes <<< map selector <<< L.take maxattempts
+
+retryIncreasingly :: forall a m. (Monad m) => Int -> Step Int -> (a -> Maybe a) -> (Int -> m (Array a)) -> m (Maybe a)
+retryIncreasingly 0 _ _ _ = pure Nothing
+retryIncreasingly _ Nil _ _ = pure Nothing
+retryIncreasingly n (Cons x xs) selector f  = do
+    value <- f x
+    case head $ catMaybes $ map selector value of
+        Nothing -> retryIncreasingly (n-1) (L.step xs) selector f
+        r@(Just _) -> pure r
+
+--retryIncreasingly 8 (step twoPows) (\x -> if x > 33 then Just x else Nothing) (\x -> logShow x *> pure x)
+
+retryUntil :: forall a m. Monad m => (a -> Maybe a) -> (Int -> m (Array a)) -> m (Maybe a)
+retryUntil = retryIncreasingly 8 (L.step twoPows)
